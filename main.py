9ac98d9d01a44556c058c3eea3e3cbf7c9eed2ce
Jeydin <p1106513@students.katyisd.org>
2022-04-13 11:41:49 -0500

    Added banner in main file

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 from functions import start
-from misc import slowprint
+from misc import slowprint, printcolor
 
 os.system("cls")
+printcolor("========================================", "cyan")
+printcolor("-----------------WELCOME----------------", "yellow")
+printcolor("========================================", "cyan")
 slowprint("Hello and welcome to my text based game! What is your name?")
 name = input(">>> ")
 start(name)
